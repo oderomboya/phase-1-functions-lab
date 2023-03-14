70842e5e0e76349b40065c5cdd5f4ad7855c322d
oderomboya <davismboya3@gmail.com>
2023-03-14 15:39:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  if (distance < 42) {
+    return 42 - distance;
+  } else {
+    return distance - 42;
+  }
+}
+
+function distanceFromHqInFeet(distance) {
+  if (distance < 42) {
+    return (42 - distance) * 264;
+  } else {
+    return (distance - 42) * 264;
+  }
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (destination > start) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+function calculatesFarePrice(start, destination) {
+  let difference;
+  start > destination
+    ? (difference = (start - destination) * 264)
+    : (difference = (destination - start) * 264);
+  if (difference < 400) {
+    return 0;
+  } else if (difference > 400 && difference < 2000) {
+    return (difference - 400) * 0.02;
+  } else if (difference > 2000 && difference < 2500) {
+    return 25;
+  } else if (difference > 2500) {
+    return "cannot travel that far";
+  }
+}
